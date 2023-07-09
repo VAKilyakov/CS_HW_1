a2614c9c8a047540b49a3b5b369385e11a27ec47
VAKilyakov <vakilyakov@bk.ru>
2023-07-09 14:49:55 +0300

    Task 1 is done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿/*
+Задача
+
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт большее число.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+/*
+/Решение
+
+Console.Write("Input number 1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number 2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("Max number is: " + number_1);
+}
+else
+{
+    Console.WriteLine("Max number is: " + number_2);
+}
+*/
+
+/*
+Тесты
+
+PS D:\Home_Works\CS_HW_1> dotnet run
+Input number 1: 5
+Input number 2: 7
+Max number is:7
+PS D:\Home_Works\CS_HW_1> dotnet run
+Input number 1: 2
+Input number 2: 10
+Max number is: 10
+PS D:\Home_Works\CS_HW_1> dotnet run
+Input number 1: -9
+Input number 2: -3
+Max number is: -3
+*/
+
+
